7126b74f46148446c686e55a3d4f7d471e1cf559
Anahi Costa <moanableu@gmail.com>
2018-08-15 20:10:24 -0400

    sharedprefs test

---
+++ app/src/main/java/com/example/android/newsapp/SettingsActivity.java
@@ -0,0 +1,63 @@
+package com.example.android.newsapp;
+
+import android.content.SharedPreferences;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+
+    public static class NewsPreferenceFragment extends PreferenceFragment
+            implements Preference.OnPreferenceChangeListener {
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings_main);
+            setHasOptionsMenu(true);
+
+            Preference mostRecent = findPreference(getString(R.string.order_by_key));
+            bindPreferenceToValue(mostRecent);
+
+            // not working
+           /* Preference pageSize = findPreference(getString(R.string.limit_page_size_key));
+            bindPreferenceToValue(pageSize);*/
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            String stringValue = newValue.toString();
+            if (preference instanceof ListPreference) {
+                ListPreference listPreference = (ListPreference) preference;
+                int preferenceIndex = listPreference.findIndexOfValue(stringValue);
+                if (preferenceIndex >= 0) {
+                    CharSequence[] labels = listPreference.getEntries();
+                    preference.setSummary(labels[preferenceIndex]);
+                }
+            } else {
+                preference.setSummary(stringValue);
+            }
+            return true;
+        }
+
+        private void bindPreferenceToValue(Preference preference) {
+            preference.setOnPreferenceChangeListener(this);
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String prefString = preferences.getString(preference.getKey(), "");
+            onPreferenceChange(preference, prefString);
+        }
+
+    }
+
+
+}
